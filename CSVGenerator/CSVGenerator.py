fd5c3e36a2680f57445af5e58f91184a4b569f3a
deadman20018 <63054160+deadman20018@users.noreply.github.com>
2023-01-05 21:13:40 +0100

    Commit on the 4th January 2023:
    Sto sempre di più provando a risolvere i problemi in autonomia e sto riuscendo sempre di più a creare degli script efficienti in pyspark.
    
    Oggi ho anche continuato a leggere il libro Learning Spark, 2nd Edition
    
    Principalmente ho ottimizato e ho create versioni alternative a degli script diversi
    
    Ho anche creato il CSVGenerator che crea un csv in base a quante colone vuoi che abbi per giocare anche con dei set di dati molto grandi (1 000 000+) di colone
    
    Oggi ho anche creato un "dataset creator" che crea dei set di dati in base a quante colonne vuoi metterci etc... domani voglio usarlo per creare dei problemi nuovi per allenarmi ad usare pyspark

--- CSVGenerator/CSVGenerator.py
+++ CSVGenerator/CSVGenerator.py
@@ -3,5 +3,6 @@ import csv
 
 with open('generatedCSV.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    for i in range(10000):
-        writer.writerow([random.randint(0,100), random.randint(0,200), random.randint(0,2000), random.randint(0,1000000)])
+    for i in range(40000):
+        print("CSV row created")
+        writer.writerow([random.randint(0,3245),random.randint(0,24352435),random.randint(0,42352435),random.randint(0,24352345),random.randint(0,23452345),random.randint(0,23452345),random.randint(0,234523452435)])
